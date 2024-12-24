52e2da18adc07e716c867f59a7f4274cd1d16f56
Leonardo Silva <106756853+leosilvajr@users.noreply.github.com>
2024-12-23 23:29:05 -0300

    Criado Tela doctos

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import Account from "./src/screens/account/account";
 import Login from "./src/screens/login/login";
+import Home from "./src/screens/home/home";
 
 export default function App() {
 
   return <>
-    <Login/>
+    <Home/>
   </>;
 }
+
+
+
+
+/*
+npm install
+npx expo start
+*/
 
---
+++ src/screens/home/home.jsx
@@ -0,0 +1,24 @@
+import { FlatList, Text, View } from "react-native"
+import { styles } from "./home.style"
+import { doctors } from "../../constants/data";
+import Doctor from "../../components/doctor/doctor";
+import icon from "../../constants/icon";
+
+function Home () {
+
+    //Criar um Container que vai ser uma View para renderizar todo o fundo da pagina.
+    return <View style={styles.container} >  
+        <Text style={styles.text}>Agende os seus serviços médicos.</Text>
+
+        <FlatList data={doctors}
+                  keyExtractor={(doc) => doc.id_doctor}
+                  showsVerticalScrollIndicator={false}
+                  renderItem={({item}) => {
+                    return <Doctor name={item.name} 
+                                   icon={item.icon === "M" ? icon.female : icon.male} 
+                                   specialty={item.specialty}/>
+                  }}/>
+     </View>
+}
+
+export default Home;
