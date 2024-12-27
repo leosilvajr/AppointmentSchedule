0458272ecce77d44e38e426452dbab420f8564b8
Leonardo Silva <106756853+leosilvajr@users.noreply.github.com>
2024-12-26 21:52:52 -0300

    Finalizado Tela de Service com a diferença nos ambientes Dev Tools e Expo Go

--- src/components/service/service.jsx
+++ src/components/service/service.jsx
@@ -1,14 +1,20 @@
 import { View, Text } from "react-native"
 import Button from "../../components/button/button";
+import { styles } from "./service.style";
 
 function Service (props){
-    return <View>
-        <View>
-            <Text >{props.description}</Text>
-            <Text >{props.price}</Text>
+    return <View style={styles.service}>
+
+        <View style={styles.containerText}>
+            <Text style={styles.description}>{props.description}</Text>
+            <Text style={styles.price}>
+                {
+                    new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(props.price)
+                }
+            </Text>
         </View>
 
-        <View>
+        <View style={styles.containerButton}>
             <Button text="Agendar" />
         </View>
     </View>
