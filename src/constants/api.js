bc4e6e79e83081045df0f685bed4362965109bca
Leonardo Silva <106756853+leosilvajr@users.noreply.github.com>
2025-01-13 08:44:06 -0300

    Ajustado base URL para endereço AWS

--- src/constants/api.js
+++ src/constants/api.js
@@ -4,8 +4,8 @@ import axios from "axios";
 //LOCAL: "http://192.168.18.4:3001"
 
 const api = axios.create({
-    //baseURL: "http://15.228.236.137:3001" //AWS
-    baseURL: "http://192.168.18.4:3001" //LOCAL
+    //baseURL: "http://192.168.18.4:3001" //LOCAL
+    baseURL: "http://15.228.236.137:3001" //AWS
 });
 
 export default api
