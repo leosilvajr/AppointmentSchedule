9c26b820326e027299adafbf94d480b76e100dc4
Leonardo Silva <106756853+leosilvajr@users.noreply.github.com>
2025-01-18 16:41:22 -0300

    Ajustado endereço para acessar API AWS

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 // //Acesso via API na AWS
-// const api = axios.create({
-//     baseURL: "http://15.228.236.137:3001" 
-// });
+const api = axios.create({
+    baseURL: "http://15.228.236.137:3001" 
+});
 
 //Acesso via LOCALHOST
-const api = axios.create({
-    baseURL: "http://192.168.18.4:3001" 
-});
+// const api = axios.create({
+//     baseURL: "http://192.168.18.4:3001" 
+// });
 
 
 export default api
